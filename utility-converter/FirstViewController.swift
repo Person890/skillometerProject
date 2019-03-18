890c51e74fa725cd72dbbd86faf851cf1452c997
Brion Mario <brion.2015283@iit.ac.lk>
2019-03-19 03:52:14 +0530

    chore: add x-code base project template

---
+++ utility-converter/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  utility-converter
+//
+//  Created by Brion Silva on 19/03/2019.
+//  Copyright © 2019 Brion Silva. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ utility-converter/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  utility-converter
+//
+//  Created by Brion Silva on 19/03/2019.
+//  Copyright © 2019 Brion Silva. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
