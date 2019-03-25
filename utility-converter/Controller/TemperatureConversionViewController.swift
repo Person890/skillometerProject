f0ab9d63407da6ef8cbfbb1c356df05dd5770389
Brion Mario <brion.2015283@iit.ac.lk>
2019-03-26 04:34:26 +0530

    chore(ui): implement temperature view ui

---
+++ utility-converter/Controller/TemperatureConversionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TemperatureConversionViewController.swift
+//  utility-converter
+//
+//  Created by Brion Silva on 26/03/2019.
+//  Copyright © 2019 Brion Silva. All rights reserved.
+//
+
+import UIKit
+
+class TemperatureConversionViewController: UIViewController {
+    
+    @IBOutlet weak var celsiusTextField: UITextField!
+    @IBOutlet weak var fahrenheitTextField: UITextField!
+    @IBOutlet weak var kelvinTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func handleCelsiusTextFieldChange(_ sender: UITextField) {
+    }
+    
+    @IBAction func handleFahrenheitTextFieldChange(_ sender: UITextField) {
+    }
+    
+    @IBAction func handleKelvinTextFieldChange(_ sender: UITextField) {
+    }
+}
